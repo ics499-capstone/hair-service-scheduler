59c28552e5dddd63667d50cfb6ded9490ae728db
kong <kmlor031@gmail.com>
2020-06-25 22:57:15 -0500

    (#4) Update UserAccount relation

--- app/models.py
+++ app/models.py
@@ -1,6 +1,14 @@
+import enum
 from app import db
+from sqlalchemy import Integer, Enum
+
+class UserAccountType(enum.Enum):
+	customer = 0
+	employee = 1
+	admin = 2
 
 class UserAccount(db.Model):
+	__tablename__ = "useraccount"
 	# primary key
 	id = db.Column(db.Integer, primary_key=True)
 	# username to log in
@@ -9,9 +17,14 @@ class UserAccount(db.Model):
 	email = db.Column(db.String(120), index=True, unique=True)
 	# sha256 hash of the password
 	password_hash = db.Column(db.String(128))
-	# phone number
-	phone_number = db.Column(db.String(20), nullable=True)
-	# the type of the account (role)
+	# account priviledge
+	type = db.Column(Enum(UserAccountType), nullable=False, default=0)
+	# register date
+	register_date = db.Column(db.DateTime, nullable=False)
+	# register confirmation
+	register_confirmed = db.Column(db.Boolean, nullable=False, default=False)
+	# register confirmation date
+	register_complete = db.Column(db.DateTime, nullable=True)
 
 	def __repr__(self):
-		return '<User {}>\n\t{}\n\t{}'.format(self.username, self.email, self.phone_number)
+		return '<UserAccount {}>\n\t{}\n\t{}'.format(self.username, self.email, self.phone_number)
