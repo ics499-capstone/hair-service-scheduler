ffe606c5ccea24bb388ea947ee653f38c734ab58
kong <kmlor031@gmail.com>
2020-06-25 21:13:43 -0500

    Python Dependency: flask-sqlalchemy

--- setup.bat
+++ setup.bat
@@ -16,4 +16,9 @@ ECHO Installing flask-migrate
 pip install flask-migrate
 ECHO ** Installing flask-migrate complete
 
+:: Install flask-sqlalchemy
+ECHO Installing flask-sqlalchemy
+pip install flask-sqlalchemy
+ECHO ** Installing flask-sqlalchemy complete
+
 :: TODO: handle python dependencies better
