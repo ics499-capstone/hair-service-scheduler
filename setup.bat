0e36f68156e57289517b959471983fad88a41e47
kong <kmlor031@gmail.com>
2020-06-26 10:44:16 -0500

    (#9) Manage python dependencies for consistency

--- setup.bat
+++ setup.bat
@@ -1,24 +1,7 @@
 @ECHO OFF
 SETLOCAL
 
-:: install flask
-ECHO Installing Flask microframework
-pip install flask
-ECHO ** Installing flask complete
-
-:: install DotEnv
-ECHO Installing DotEnv
-pip install python-dotenv
-ECHO ** Installing python-dotenv complete
-
-:: Install flask-migrate
-ECHO Installing flask-migrate
-pip install flask-migrate
-ECHO ** Installing flask-migrate complete
-
-:: Install flask-sqlalchemy
-ECHO Installing flask-sqlalchemy
-pip install flask-sqlalchemy
-ECHO ** Installing flask-sqlalchemy complete
-
-:: TODO: handle python dependencies better
+:: install dependencies
+ECHO ** Installing Python dependencies
+pip install -r requirements.txt
+ECHO ** Installation complete
