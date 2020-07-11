a9b9c5ddea4c7a0787ba12f2801f278d23ce37a3
kong <kmlor031@gmail.com>
2020-07-11 12:10:41 -0500

    (#13) API: /api/user/register - complete insertion of accounts

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import os
 from flask_cors import CORS
+from .models import UserAccount
+
 
 # instantiate the application, database and migration
 app = Flask(__name__)
@@ -15,16 +17,40 @@ CORS(app)
 
 @app.route('/api/user/register', methods=['POST'])
 def register():
-  json = request.get_json()
-  transaction_keys = ['username' , 'email', 'password', 'passwordConfirm']
+  try:
+    json = request.get_json()
+    transaction_keys = ['username' , 'email', 'password', 'passwordConfirm']
 
-  if not all (key in json for key in transaction_keys):
-    return 'Something Missing' , 400
+    if not all (key in json for key in transaction_keys):
+      return '400 Bad Request - missing fields', 400
 
-  email = request.get_json()['email']
-  print(email)
-  # return results
-  result = {
-    "email": "asd"
-  }
-  return jsonify({"results": result}), 201
+    username = request.get_json()['username']
+    email = request.get_json()['email']
+    password = request.get_json()['password']
+    passwordConfirm = request.get_json()['passwordConfirm']
+
+    # check if there is already a user logged in
+    # check if useraccount already exist
+    useraccount = UserAccount.query.filter_by(username=username).first()
+    if useraccount:
+      return '409 User already exist', 409
+
+    # check if the password is equal to passwordConfirm
+    if password != passwordConfirm:
+      return '401 Password mismatch', 401
+
+    # create the useraccount
+    newaccount = UserAccount(username, email, password)
+
+    # add the user to database
+    db.session.add(newaccount)
+    db.session.commit()
+
+    # return some info back to client
+    # return results
+    result = {
+      "status": "asd"
+    }
+    return jsonify({"results": result}), 201
+  except:
+    return '503 Service Unavailable', 503
--- app/models.py
+++ app/models.py
@@ -1,30 +1,42 @@
 import enum
-from app import db
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Integer, Enum
+from werkzeug.security import generate_password_hash
+from datetime import datetime
+
+db = SQLAlchemy()
 
 class UserAccountType(enum.Enum):
-	customer = 0
-	employee = 1
-	admin = 2
+  customer = 0
+  employee = 1
+  admin = 2
 
 class UserAccount(db.Model):
-	__tablename__ = "useraccount"
-	# primary key
-	id = db.Column(db.Integer, primary_key=True)
-	# username to log in
-	username = db.Column(db.String(64), index=True, unique=True)
-	# email address for registering
-	email = db.Column(db.String(120), index=True, unique=True)
-	# sha256 hash of the password
-	password_hash = db.Column(db.String(128))
-	# account priviledge
-	type = db.Column(Enum(UserAccountType), nullable=False, default=0)
-	# register date
-	register_date = db.Column(db.DateTime, nullable=False)
-	# register confirmation
-	register_confirmed = db.Column(db.Boolean, nullable=False, default=False)
-	# register confirmation date
-	register_complete = db.Column(db.DateTime, nullable=True)
+  __tablename__ = "useraccount"
+  # primary key
+  id = db.Column(db.Integer, primary_key=True)
+  # username to log in
+  username = db.Column(db.String(64), index=True, unique=True)
+  # email address for registering
+  email = db.Column(db.String(120), index=True, unique=True)
+  # sha256 hash of the password
+  password_hash = db.Column(db.String(128))
+  # account priviledge
+  type = db.Column(Enum(UserAccountType), nullable=False, default=0)
+  # register date
+  register_date = db.Column(db.DateTime, nullable=False)
+  # register confirmation
+  register_confirmed = db.Column(db.Boolean, nullable=False, default=False)
+  # register confirmation date
+  register_complete = db.Column(db.DateTime, nullable=True, default=False)
 
-	def __repr__(self):
-		return '<UserAccount {}>\n\t{}\n\t{}'.format(self.username, self.email, self.phone_number)
+  # contructor
+  def __init__(self, username, email, password):
+    self.username = username
+    self.email = email
+    self.password_hash = generate_password_hash(password, method='sha256')
+    self.type = UserAccountType.customer
+    self.register_date = datetime.now()
+
+  def __repr__(self):
+    return '<UserAccount {}>\n\t{}\n\t{}'.format(self.username, self.email, self.phone_number)
