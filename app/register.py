acf1fb45b4426cc8e66e8d4fc24305ebae275f1e
kong <kmlor031@gmail.com>
2020-07-11 13:42:10 -0500

    (#13) move register function to seperate file

---
+++ app/register.py
@@ -0,0 +1,46 @@
+from flask import Flask, Blueprint, jsonify, request, json
+from .models import UserAccount
+
+register_bp = Blueprint('register', __name__)
+
+@register_bp.route('/api/user/register', methods=['POST'])
+def register():
+
+    json = request.get_json()
+    transaction_keys = ['username' , 'email', 'password', 'passwordConfirm']
+
+    if not all (key in json for key in transaction_keys):
+      return '400 Bad Request - missing fields', 400
+
+    username = request.get_json()['username']
+    email = request.get_json()['email']
+    password = request.get_json()['password']
+    passwordConfirm = request.get_json()['passwordConfirm']
+
+    # check if there is already a user logged in
+    # check if useraccount already exist
+    useraccount_username = UserAccount.query.filter_by(username=username).first()
+    if  useraccount_username:
+      return '409 User already exist', 409
+
+    # check if email already exist
+    useraccount_email = UserAccount.query.filter_by(email=email).first()
+    if useraccount_email:
+      return '409 Existing account with associated email', 409
+
+    # check if the password is equal to passwordConfirm
+    if password != passwordConfirm:
+      return '401 Password mismatch', 401
+
+    # create the useraccount
+    newaccount = UserAccount(username, email, password)
+
+    # add the user to database
+    db.session.add(newaccount)
+    db.session.commit()
+
+    # tell the user to complete registration through email
+    result = {
+      "status": "asd"
+    }
+    return jsonify({"results": result}), 201
