7ba1b8cbb3dcea5ccbb5a58c3e3183f7cee35e20
kong <kmlor031@gmail.com>
2020-08-15 12:17:26 -0500

    (#37) Send all services to the frontend

--- flaskr/data.py
+++ flaskr/data.py
@@ -10,9 +10,10 @@ API_URL = '/api/data/'
 
 bp = Blueprint('data', __name__, url_prefix=API_URL)
 
+# this endpoint sends all product to the frontend
 @bp.route('/product', methods=['GET'])
 def product():
-  from flaskr.models import db, Product, UserAccount, ShoppingCart
+  from flaskr.models import db, Product
   
   query = Product.query
   data = query.all()
@@ -28,3 +29,22 @@ def product():
     })
 
   return jsonify(product=arr), status.HTTP_200_OK
+
+# this endpoint sends all the available services to the frontend
+@bp.route('/service', methods=['GET'])
+def service():
+  from flaskr.models import db, Service
+  
+  query = Service.query
+  data = query.all()
+
+  arr = [] # array of dictionary
+  for i in data:
+    arr.append({
+      'id' : i.id,
+      'name' : i.name,
+      'description' : i.description,
+      'price' : float(i.price)
+    })
+
+  return jsonify(service=arr), status.HTTP_200_OK
